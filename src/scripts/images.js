671aad2dd07c7337e6212d1cfd4ae55006010649
Juliana <jumiranda0501@gmail.com>
2024-01-17 18:54:58 -0300

    Initial commit

---
+++ src/scripts/images.js
@@ -0,0 +1,17 @@
+'use-strict';
+
+import LogoDark from '../assets/images/logo-dark.svg';
+import LogoLight from '../assets/images/logo-light.svg';
+import Phone from '../assets/images/image-hero.webp';
+import Founder from '../assets/images/image-founder.webp';
+import Pattern1 from '../assets/images/bg-pattern-1.svg';
+import Pattern2 from '../assets/images/bg-pattern-2.svg';
+import Pattern3 from '../assets/images/bg-pattern-3.svg';
+
+document.getElementById('header-logo').src = LogoLight;
+document.getElementById('footer-logo').src = LogoDark;
+document.getElementById('hero-img').src = Phone;
+document.getElementById('founder-img').src = Founder;
+document.getElementById('pattern-1').src = Pattern1;
+document.getElementById('pattern-2').src = Pattern2;
+document.getElementById('pattern-3').src = Pattern3;
